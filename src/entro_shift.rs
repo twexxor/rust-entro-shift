5daf41c5d762f4a3e778b00ad12699947ea7b92f
Will Parsons <entrocraft@gmail.com>
2023-12-30 01:15:14 +0000

    Adding the missing second bitwise multiplication operation.

--- src/entro_shift.rs
+++ src/entro_shift.rs
@@ -3,6 +3,7 @@ pub fn entro_shift(mut entropy: u32) -> u32 {
     entropy = (!(entropy ^ 1111111111)).wrapping_sub(!entropy << 5);
     entropy = (entropy << 31).wrapping_add(!entropy >> 1);
     entropy = (entropy << 3).wrapping_add(entropy);
+    entropy = (entropy << 3).wrapping_add(entropy);
     entropy ^= 1111111111;
     entropy = (!entropy << 5).wrapping_sub(entropy);
     return (entropy >> 1) ^ entropy;
